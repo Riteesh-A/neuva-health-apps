97630f6576ccf5f961162dcd3fbe8baf6d687f77
Tejas Ladhe <tejasladhe24@gmail.com>
2025-03-14 19:34:12 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { hankenGrotesk } from "@/core/fonts";
 import { baseUrl, cn } from "@/core/lib/utils";
 import { ThemeProvider } from "@/core/provider/theme-provider";
-import { siteConfig } from "@/site-config";
+import { companyName, siteConfig } from "@/site-config";
 import { ReactNode } from "react";
 import { Toaster } from "sonner";
 import "./globals.css";
@@ -27,6 +27,35 @@ export const metadata = {
       href: "/favicon.png",
     },
   ],
+  openGraph: {
+    title: siteConfig.title,
+    description: siteConfig.description,
+    url: siteConfig.url,
+    siteName: companyName,
+    images: [
+      {
+        url: "/screenshots/mobile-preview.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Neuva Health",
+      },
+    ],
+    locale: siteConfig.locale,
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@NeuvaHealth",
+    title: siteConfig.title,
+    description: siteConfig.description,
+    images: ["/screenshots/mobile-preview.jpeg"],
+  },
+  alternates: {
+    canonical: baseUrl,
+    languages: {
+      "en-IN": baseUrl,
+    },
+  },
 };
 
 export default async function RootLayout({
